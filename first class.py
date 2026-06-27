640422360b175d695de2e51b102c83236a92eb1f
0xSAHIR <0xsahir@gmail.com>
2026-06-27 06:25:32 -0700

    Add files via upload
    this was the first class work for computer programming in we just learned how does commands run and being output, and it was just basic and just a start.

---
+++ first class.py
@@ -0,0 +1,102 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a='muhammad'
+b='amna'
+c='shafay'
+string='a,b,c'
+string=('a\tb\tc')
+print(string)
+a	b	c
+
+print('a,b,c')
+a,b,c
+a
+'muhammad'
+b
+'amna'
+c
+'shafay'
+print('a\tb\tc')
+a	b	c
+print('muhammad,amna,shafay')
+muhammad,amna,shafay
+print('muhammad\tamna\tshafay')
+muhammad	amna	shafay
+s='this is class'
+s[3:5]+s[11:]+s[:-1]
+'s ssthis is clas'
+len(s)
+13
+if 5>4
+SyntaxError: expected ':'
+if 5>4:
+    print(true)
+
+    
+Traceback (most recent call last):
+  File "<pyshell#20>", line 2, in <module>
+    print(true)
+NameError: name 'true' is not defined. Did you mean: 'True'?
+if 5>4:
+    print('true')
+... 
+...     
+true
+>>> else :
+...     
+SyntaxError: invalid syntax
+>>> else:
+...     
+SyntaxError: invalid syntax
+>>> else
+SyntaxError: invalid syntax
+>>> else if :
+...     
+SyntaxError: invalid syntax
+>>> else if
+SyntaxError: invalid syntax
+>>> else if print('false')
+SyntaxError: invalid syntax
+>>> 
+>>> i=5
+>>> for i
+SyntaxError: invalid syntax
+>>> for i 1 TO 10 :
+...     
+SyntaxError: invalid syntax
+>>> for:i:1to10
+SyntaxError: invalid decimal literal
+>>> for:i:1 to 10
+SyntaxError: invalid syntax
+>>> for:i
+SyntaxError: invalid syntax
+>>> x=str('5')
+>>> x
+'5'
+>>> y=int(6)
+>>> y
+6
+>>> z=float('7')
+>>> z
+7.0
+>>> print('x+y+z')
+x+y+z
+>>> print(x+y+z)
+Traceback (most recent call last):
+  File "<pyshell#44>", line 1, in <module>
+    print(x+y+z)
+TypeError: can only concatenate str (not "int") to str
+>>> print('x,y,z')
+x,y,z
+>>> my_variable_name='Jhonny'
+>>> a,b,c='amna','muhammad','shafay'
+>>> print('a\tb\tc')
+a	b	c
+>>> print(a\tb\tc)
+SyntaxError: unexpected character after line continuation character
+>>> a
+'amna'
+>>> b
+'muhammad'
+>>> c
+'shafay'
